ad6c47437311d42e66f5d3f08b0f1483c0ff6550
Rukev001 <112125156+Rukev001@users.noreply.github.com>
2022-09-25 06:08:48 +0100

    Update config.js

--- config.js
+++ config.js
@@ -7,13 +7,13 @@ global.autoReadAll = false
 global.antitags = true
 
 
-global.Owner = ['918101187835']
-global.OwnerNumber = ['918101187835']
-global.ownertag = ['918101187835']
-global.BotName = "Miku"
-global.packname = "Miku bot"
-global.author = "By: Fantox"
-global.OwnerName = "Fantox"
+global.Owner = ['2349066967525']
+global.OwnerNumber = ['2349066967525']
+global.ownertag = ['2349066967525']
+global.BotName = "Rukev"
+global.packname = "Rukev bot"
+global.author = "By: Rukev"
+global.OwnerName = "Rukev"
 global.BotSourceCode = "https://github.com/FantoX001/Miku-MD"
 global.SupportGroupLink = "https://chat.whatsapp.com/JcBzjRIxM5UHfuJbt0KrWt"
 global.sessionName = "session"
@@ -59,17 +59,17 @@ global.ntilink = []
 
 global.mess = {
     jobdone: 'Job done...',
-    useradmin: 'Sorry, only *Group Admins* can use this command *Baka*!',
+    useradmin: 'Sorry, only *Group Admins* can use this command *WAKA*!',
     botadmin: 'Sorry, i cant execute this command without being an *Admin* of this group.',
-    botowner: 'Only my *Owner* can use this command, Baka!',
-    grouponly: 'This command is only made for *Groups*, Baka!',
-    privateonly: 'This command is only made for *Private Chat*, Baka!',
+    botowner: 'Only my *Owner* can use this command, THUNDER FIRE YOU!',
+    grouponly: 'This command is only made for *Groups*, WAKA!',
+    privateonly: 'This command is only made for *Private Chat*, WEREY!',
     botonly: 'Only the *Bot itself* can use this command!',
-    waiting: 'Chotto Matte...',
+    waiting: 'Chill Rukev bot is processing...',
     nolink: 'Please provide me *link*, Baka!',
     error: 'An error occurd!',
-    banned: 'You are *Banned* fron using commands!',
-    bangc: 'This Group is *Banned* from using Commands!',
+    banned: 'You are *Banned* fron using commands Waka dere!',
+    bangc: 'This Group is *Banned* from using Commands ogun kill una!',
     nonsfw: 'Dont be a pervert Baka! This is not a NSFW enabled group!'
     
 }
